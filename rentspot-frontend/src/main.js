708cce32ffd44b0d4b959790c741eae730af8268
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-05-17 03:21:15 +0930

    BackendIntergration, Propertyjs, Navbar|Login|Register add refresh and delete alert

--- rentspot-frontend/src/main.js
+++ rentspot-frontend/src/main.js
@@ -2,8 +2,22 @@ import { createApp } from 'vue';
 import Home from '../src/views/home.vue';
 import App from '../src/App.vue';
 import router from '../router/index.js';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import '../src/style.css';
+import { POSITION } from 'vue-toastification';
 
 const app = createApp(App);
 app.use(router);
+app.use(Toast, {
+  position: POSITION.TOP_CENTER,
+  timeout: 2000,
+  transition: 'Vue-Toastification__bounce',
+  closeOnClick: true,
+  pauseOnHover: true,
+  hideProgressBar: false,
+
+  toastClassName: ['Vue-Toastification__toast', 'rentspot-toast'],
+  bodyClassName: 'rentspot-toast-body'
+});
 app.mount('#app');
